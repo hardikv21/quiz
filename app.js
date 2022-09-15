a5d8694d05251f05d7b31acd1fa984d6a9d88bae
Hardik Vora <hardik7304@gmail.com>
2022-09-14 21:21:22 -0400

    Commit reverted

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ MongoClient    = require('mongodb').MongoClient,
 mongoURL       = "mongodb+srv://hardik:hardik21@atlascluster.p8emwjg.mongodb.net/?retryWrites=true&w=majority",
 PORT           = process.env.PORT || 4200,
 authRouter     = require("./routes"),
-quizRouter     = require("./routes/quiz"),
 middleware     = require("./middleware");
 
 require("dotenv").config();
@@ -60,92 +59,91 @@ app.use((req, res, next) => {
 });
 
 app.use("/", authRouter);
-app.use("/", quizRouter);
 
-// const filterQuestions = (questions, filterType) => {
-//     return questions.filter((question) => question.type === filterType);
-// };
+const filterQuestions = (questions, filterType) => {
+    return questions.filter((question) => question.type === filterType);
+};
 
-// const calculateScore = (response) => {
-//     let quizScore = 0; 
-//     for (const property in response) {
-//         if (eval(property) == response[property]) {
-//             quizScore += 10;
-//         }
-//     }
-//     return quizScore;
-// }
+const calculateScore = (response) => {
+    let quizScore = 0; 
+    for (const property in response) {
+        if (eval(property) == response[property]) {
+            quizScore += 10;
+        }
+    }
+    return quizScore;
+}
 
-// MongoClient.connect(mongoURL)
-//     .then((response) => {
-//         const quizDB = response.db("quiz");
+MongoClient.connect(mongoURL)
+    .then((response) => {
+        const quizDB = response.db("quiz");
         
-//         app.get(
-//             "/sum-quiz",
-//             middleware,
-//             (req, res) => {
-//                 quizDB.collection("question").find().toArray()
-//                     .then((response) =>
-//                         res.render(
-//                             "quiz",
-//                             {
-//                                 questions: filterQuestions(response, "sum"),
-//                                 quizType: "sum"
-//                             }
-//                         )
-//                     )
-//                     .catch((error) => console.log(error));
-//             }
-//         );
+        app.get(
+            "/sum-quiz",
+            middleware,
+            (req, res) => {
+                quizDB.collection("question").find().toArray()
+                    .then((response) =>
+                        res.render(
+                            "quiz",
+                            {
+                                questions: filterQuestions(response, "sum"),
+                                quizType: "sum"
+                            }
+                        )
+                    )
+                    .catch((error) => console.log(error));
+            }
+        );
 
-//         app.post(
-//             "/sum-quiz",
-//             middleware,
-//             (req, res) => (
-//                 res.render(
-//                     "result",
-//                     {
-//                         quizType: "Sum",
-//                         quizScore: calculateScore(req.body)
-//                     }
-//                 )
-//             )
-//         );
+        app.post(
+            "/sum-quiz",
+            middleware,
+            (req, res) => (
+                res.render(
+                    "result",
+                    {
+                        quizType: "Sum",
+                        quizScore: calculateScore(req.body)
+                    }
+                )
+            )
+        );
 
-//         app.get(
-//             "/minus-quiz",
-//             middleware,
-//             (req, res) => {
-//                 quizDB.collection("question").find().toArray()
-//                     .then((response) =>
-//                         res.render(
-//                             "quiz",
-//                             {
-//                                 questions: filterQuestions(response, "minus"),
-//                                 quizType: "minus"
-//                             }
-//                         )
-//                     )
-//                     .catch((error) => console.log(error));
-//             }
-//         );
+        app.get(
+            "/minus-quiz",
+            middleware,
+            (req, res) => {
+                quizDB.collection("question").find().toArray()
+                    .then((response) =>
+                        res.render(
+                            "quiz",
+                            {
+                                questions: filterQuestions(response, "minus"),
+                                quizType: "minus"
+                            }
+                        )
+                    )
+                    .catch((error) => console.log(error));
+            }
+        );
 
-//         app.post(
-//             "/minus-quiz",
-//             middleware,
-//             (req, res) => {
-//                 res.render(
-//                     "result",
-//                     {
-//                         quizType: "Minus",
-//                         quizScore: calculateScore(req.body)
-//                     }
-//                 )
-//             }
-//         );
-//     })
-//     .catch((error) => console.log(error));
+        app.post(
+            "/minus-quiz",
+            middleware,
+            (req, res) => {
+                res.render(
+                    "result",
+                    {
+                        quizType: "Minus",
+                        quizScore: calculateScore(req.body)
+                    }
+                )
+            }
+        );
+    })
+    .catch((error) => console.log(error));
 
-// app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}:`));
